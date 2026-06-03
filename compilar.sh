d5d6391f81643e4b0a6e286f3d13c970273b4104
Martina <mnudel@itba.edu.ar>
2026-06-03 16:36:07 -0300

    chore: actualizar contenedor de compilacion

--- compilar.sh
+++ compilar.sh
@@ -1,6 +1,6 @@
-docker start ARQUI-TPE
-docker exec -it ARQUI-TPE make clean -C /root/Toolchain
-docker exec -it ARQUI-TPE make clean -C /root/
-docker exec -it ARQUI-TPE make -C /root/Toolchain
-docker exec -it ARQUI-TPE make -C /root/
-docker stop ARQUI-TPE
+docker start TPE2-SO
+docker exec -it TPE2-SO make clean -C /root/Toolchain
+docker exec -it TPE2-SO make clean -C /root/
+docker exec -it TPE2-SO make -C /root/Toolchain
+docker exec -it TPE2-SO make -C /root/
+docker stop TPE2-SO
